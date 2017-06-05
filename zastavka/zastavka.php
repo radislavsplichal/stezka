2fd4dc077331ae83d0cbdcc5ae3b3cc50e4ef01f
Calipheos <kirin.bezny@gmail.com>
2017-06-05 22:54:18 +0200

    DB marks, css separation

--- zastavka/zastavka.php
+++ zastavka/zastavka.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Zastavení IV</title>
+  <title>DBTitle Zastavení IV</title>
 
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -13,13 +13,7 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.18/jquery.touchSwipe.js" integrity="sha256-KKv6+21Uvs0xGccvranH3/RaUvz7N24khCbpW6leF5o=" crossorigin="anonymous"></script>
   <script type="text/javascript" src="ckeditor/ckeditor.js"></script>
-
-  <style>
-  .pict {
-      display: block;
-      margin: auto;
-  }
-  </style>
+  <link rel="stylesheet" href="carouselSwipe.css">
 </head>
 <body>
 
@@ -41,7 +35,7 @@
                     <span class="sr-only">Next</span>
                   </a>
                 </div>
-                <h2>Zastaveni IV</h2>
+                <h2>DBTitle Zastaveni IV</h2>
 
               </div>
 
@@ -53,15 +47,16 @@
           <!-- Wrapper for slides -->
           <div class="carousel-inner" role="listbox">
             <div class="item active">
-              <img class="pict" src="http://www.farnostzebetin.cz/wp-content/gallery/krizova-cesta-v-kostele-sv-bartolomeje/KC-04.jpg">
+              <img class="pict" src="http://www.farnostzebetin.cz/wp-content/gallery/krizova-cesta-v-kostele-sv-bartolomeje/KC-04.jpg" alt="DBPicture">
             </div>
             <div class="item">
-              <h2>Zastavení IV: "Ná, tady máš svůj hřebík..."</h2>
+              <h2>DBInfoTitle Zastavení IV: "Ná, tady máš svůj hřebík..."</h2>
+              <p>DBInfoText<p>
               <p>Toto zobrazení připomíná toho malýho zmetka, co našel Ježíšův "ztracený" hřebík.</p>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempor vel ante non suscipit. Ut ex felis, scelerisque in nulla vitae, viverra aliquam dui. Sed lacus leo, pellentesque id cursus id, fermentum eget arcu. In vel felis et nibh scelerisque finibus ac non risus. Sed sed dignissim mi. Morbi sagittis eros nec porta pretium. Curabitur fermentum leo quis justo vestibulum, ac commodo diam vulputate. Donec in congue justo. Nam blandit tincidunt justo sed rutrum.</p>
               <p>Pellentesque id est a libero fringilla laoreet. Phasellus ac tincidunt dui. Sed auctor tortor vitae arcu posuere feugiat. Duis at pellentesque metus. Nunc condimentum at lorem bibendum malesuada. Nullam ac ante lectus. Fusce id urna sed nunc fermentum sagittis. Vivamus ac egestas diam. Sed interdum turpis in ante cursus, sed iaculis nulla blandit. Quisque eleifend odio eu augue tincidunt, semper sollicitudin leo ultricies. Sed commodo eros eu hendrerit bibendum. Pellentesque luctus gravida tellus in tincidunt. Nullam venenatis congue magna, quis lobortis augue luctus sit amet.</p>
               <p>Duis sit amet congue enim, id pellentesque mauris. Fusce a lacinia neque, a pellentesque quam. Integer interdum rhoncus augue, dictum varius ligula finibus at. Vestibulum sed mauris eu velit volutpat ultricies. Nam euismod ac ligula sed vestibulum. Ut molestie, lacus vel ultrices vestibulum, felis tortor mollis ante, sed blandit orci lectus vitae dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum metus nibh, eget eleifend velit vehicula in. Quisque nec tortor turpis. Aliquam sollicitudin, odio sit amet tincidunt varius, neque felis accumsan metus, in blandit quam nibh quis purus. Mauris sapien dui, dictum in rutrum eget, varius quis eros.</p>
-              <p><b>KÁMEN</b></p>
+              <p>KÁMEN</p>
             </div>
           </div>
 
