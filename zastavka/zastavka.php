9dbc93959fa1f10b1d027aeb2ba56f3811b43c02
Calipheos <kirin.bezny@gmail.com>
2017-06-05 22:36:39 +0200

    IT IS WORKING!!! :-D
    Swipe page change, enlarging images on mobile devices

--- zastavka/zastavka.php
+++ zastavka/zastavka.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>My Blog</title>
+  <title>Zastavení IV</title>
 
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -11,100 +11,67 @@
 
   <!-- Latest compiled JavaScript -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.18/jquery.touchSwipe.js" integrity="sha256-KKv6+21Uvs0xGccvranH3/RaUvz7N24khCbpW6leF5o=" crossorigin="anonymous"></script>
   <script type="text/javascript" src="ckeditor/ckeditor.js"></script>
-  <script>
-  $(".carousel").swipe({
 
-  swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
-
-    if (direction == 'left') $(this).carousel('next');
-    if (direction == 'right') $(this).carousel('prev');
-
-  },
-  allowPageScroll:"vertical"
-
-});
-  </script>
+  <style>
+  .pict {
+      display: block;
+      margin: auto;
+  }
+  .carousel-inner {
+      min-height: 100%;
+  }
+  </style>
 </head>
 <body>
 
-
-
-
-
-<div class="container">
-  <div class="row">
+<div class="container-fluid text-center">
 
       <div class="thumbnail">
-        <img src="http://www.farnostzebetin.cz/wp-content/gallery/krizova-cesta-v-kostele-sv-bartolomeje/KC-04.jpg" alt="...">
         <div class="caption">
-          <h3 class="text-center">Zastaveni 1.... whatevs'</h3>
+
+              <div class="carousel-controls">
+                <div class="pull-left">
+                  <a href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon glyphicon-arrow-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                </div>
+                <div class="pull-right">
+                  <a href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
+                <h2>Zastaveni IV</h2>
+
+              </div>
+
+        </div>
+
+
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" data-interval="false">
+
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner" role="listbox">
+            <div class="item active">
+              <img class="pict" src="http://www.farnostzebetin.cz/wp-content/gallery/krizova-cesta-v-kostele-sv-bartolomeje/KC-04.jpg">
+            </div>
+            <div class="item">
+              <h2>Zastavení IV: "Ná, tady máš svůj hřebík..."</h2>
+              <p>Toto zobrazení připomíná toho malýho zmetka, co našel Ježíšův "ztracený" hřebík.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempor vel ante non suscipit. Ut ex felis, scelerisque in nulla vitae, viverra aliquam dui. Sed lacus leo, pellentesque id cursus id, fermentum eget arcu. In vel felis et nibh scelerisque finibus ac non risus. Sed sed dignissim mi. Morbi sagittis eros nec porta pretium. Curabitur fermentum leo quis justo vestibulum, ac commodo diam vulputate. Donec in congue justo. Nam blandit tincidunt justo sed rutrum.</p>
+              <p>Pellentesque id est a libero fringilla laoreet. Phasellus ac tincidunt dui. Sed auctor tortor vitae arcu posuere feugiat. Duis at pellentesque metus. Nunc condimentum at lorem bibendum malesuada. Nullam ac ante lectus. Fusce id urna sed nunc fermentum sagittis. Vivamus ac egestas diam. Sed interdum turpis in ante cursus, sed iaculis nulla blandit. Quisque eleifend odio eu augue tincidunt, semper sollicitudin leo ultricies. Sed commodo eros eu hendrerit bibendum. Pellentesque luctus gravida tellus in tincidunt. Nullam venenatis congue magna, quis lobortis augue luctus sit amet.</p>
+              <p>Duis sit amet congue enim, id pellentesque mauris. Fusce a lacinia neque, a pellentesque quam. Integer interdum rhoncus augue, dictum varius ligula finibus at. Vestibulum sed mauris eu velit volutpat ultricies. Nam euismod ac ligula sed vestibulum. Ut molestie, lacus vel ultrices vestibulum, felis tortor mollis ante, sed blandit orci lectus vitae dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum metus nibh, eget eleifend velit vehicula in. Quisque nec tortor turpis. Aliquam sollicitudin, odio sit amet tincidunt varius, neque felis accumsan metus, in blandit quam nibh quis purus. Mauris sapien dui, dictum in rutrum eget, varius quis eros.</p>
+              <p><b>KÁMEN</b></p>
+            </div>
+          </div>
+
         </div>
       </div>
-
-
-
-      <div class="container">
-
-        <div class="page-header">
-          <h1>Bootstrap default carousel with swipe and touch gestures</h1>
-          <p class="lead">How to add the swipes gesture to comand the Bootstrap carousel on mobile devices.</p>
-          <p>Made by <strong>Andrea Rufo</strong>, more info and tutorial on <a href="http://www.orangedropdesign.com/"> OrangeDropDesign.com</a></p>
-        </div>
-
-      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="4"></li>
-        </ol>
-
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-          <div class="item active">
-            <img src="http://www.farnostzebetin.cz/wp-content/gallery/krizova-cesta-v-kostele-sv-bartolomeje/KC-04.jpg">
-          </div>
-          <div class="item">
-            <img src="http://www.farnostzebetin.cz/wp-content/gallery/krizova-cesta-v-kostele-sv-bartolomeje/KC-04.jpg">
-          </div>
-          <div class="item">
-            <img src="http://www.farnostzebetin.cz/wp-content/gallery/krizova-cesta-v-kostele-sv-bartolomeje/KC-04.jpg">
-          </div>
-          <div class="item">
-            <img src="http://www.farnostzebetin.cz/wp-content/gallery/krizova-cesta-v-kostele-sv-bartolomeje/KC-04.jpg">
-          </div>
-          <div class="item">
-            <img src="http://www.farnostzebetin.cz/wp-content/gallery/krizova-cesta-v-kostele-sv-bartolomeje/KC-04.jpg">
-          </div>
-        </div>
-
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-
-    </div>
-
-
-
-
   </div>
-</div>
-
-
-
-
-
-
 
 </body>
 </html>
+<script src="carouselSwipe.js"></script>
