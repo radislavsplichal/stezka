1139d8f8a698876e0bf8dff4b6de4bf5f39d81c5
Calipheos <kirin.bezny@gmail.com>
2017-06-05 23:15:43 +0200

    background and font colour change, font scale

--- zastavka/zastavka.php
+++ zastavka/zastavka.php
@@ -5,12 +5,13 @@
   <title>DBTitle Zastavení IV</title>
 
   <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- jQuery library -->
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-  <!-- Latest compiled JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.18/jquery.touchSwipe.js" integrity="sha256-KKv6+21Uvs0xGccvranH3/RaUvz7N24khCbpW6leF5o=" crossorigin="anonymous"></script>
   <script type="text/javascript" src="ckeditor/ckeditor.js"></script>
   <link rel="stylesheet" href="carouselSwipe.css">
@@ -19,7 +20,7 @@
 
 <div class="container-fluid text-center">
 
-      <div class="thumbnail">
+      <div class="row">
         <div class="caption">
 
               <div class="carousel-controls">
